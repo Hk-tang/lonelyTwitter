b3e32b3976e318157fc5ecd40ae6d6ad928e2d7c
Hk-tang <hktang@ualberta.ca>
2017-01-17 19:25:46 -0700

    Lab2Exercise

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,48 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by hktang on 1/17/17.
+ */
+
+public abstract class Tweet implements Tweetable{
+    private Date date; //This is date for Tweet class
+    private String message;
+    private String hiddenString;
+
+    public Tweet(Date date, String message) throws TweetTooLongException {
+        this.date = date; //This is date for constructor
+        this.setMessage(message);
+    }
+
+    public Tweet(String message) throws TweetTooLongException {
+        this.setMessage(message);
+        //this.message = message;
+        this.date = new Date(); //current time and date
+    }
+
+    public abstract Boolean isImportant(); //Child classes must implement "isImportant"
+    // Also allows you to keep using if you upcast
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) throws TweetTooLongException {
+        if(message.length() > 144){
+            throw new TweetTooLongException();
+        }
+        else{
+            this.message = message;
+        }
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/importantTweet.java
@@ -0,0 +1,27 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by hktang on 1/17/17.
+ */
+
+public class importantTweet extends Tweet {
+
+    public importantTweet(Date date, String message) throws TweetTooLongException {
+        super(date, message);
+    }
+
+    public importantTweet(String message) throws TweetTooLongException {
+        super(message);
+    }
+
+    public Boolean isImportant(){
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + " !!!!";
+    }
+}
